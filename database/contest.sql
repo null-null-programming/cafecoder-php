0bec503f113b18ff68eecb9bd88277f2d4b53f7a
douro <piyopiyo51@gmail.comO>
2019-11-23 11:03:12 +0900

    データベース再編

---
+++ database/contest.sql
@@ -0,0 +1,21 @@
+CREATE TABLE kakecoder.contests(
+  contest_id varchar(32) NOT NULL,
+  contest_name varchar(32) NOT NULL,
+  start_time datetime NOT NULL,
+  end_time datetime NOT NULL,
+ PRIMARY KEY (contest_id)
+);
+CREATE TABLE kakecoder.problem(
+  contest_id varchar(32) NOT NULL,
+  problem varchar(4) NOT NULL,
+  point int,
+  testcase_path varchar(255),
+  PRIMARY KEY (contest_id, problem)
+);
+CREATE TABLE kakecoder.uploads (
+    code_session varchar(32) NOT NULL,
+    problem varchar(1) NOT NULL,
+    user_id varchar(32) NOT NULL,
+    upload_date datetime,
+    PRIMARY KEY (code_session)
+);
