1770979368228ccc96cf4f4ad979b9deab00e2ee
earlgray283 <earlgray283@gmail.com>
2019-11-24 18:52:52 +0900

    tea003

--- public/tea003/index.php
+++ public/tea003/index.php
@@ -1,16 +1,24 @@
 <!doctype html>
 <html lang="ja">
+
 <head>
-<?php include_once "../template/head.php"; ?>
+    <?php include_once "../../template/head.php"; ?>
 </head>
+
 <body>
-<?php include_once "../template/nav.php"; ?>
+    <?php include_once "../../template/nav.php"; ?>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous">
+    </script>
 
 
 
@@ -22,7 +30,8 @@
             <div class="card-body">
                 <nav class="navbar navbar-expand-sm navbar-light bg-light">
 
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navmenu1" aria-controls="navmenu1" aria-expanded="false" aria-label="Toggle navigation">
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navmenu1"
+                        aria-controls="navmenu1" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navmenu1">
@@ -47,7 +56,7 @@
                 <br>
                 <p class="card-text">
                     ・開催時刻：2019-11-26(火) 21:00:00<br>
-                    ・開催時間：90分<br>
+                    ・開催時間：100分<br>
                     ・問題数：6問<br>
                     ・writer：otamay, Ultraviolett, sakaki_tohru<br>
                     ・ペナルティ：<s>3分</s>なし<br>
@@ -59,7 +68,7 @@
                     　※提出する画面は問題文ページに統合されました。<br>
                     　　提出する際は問題文ページの一番下から提出してください。<br>
                 </p>
-               
+
 
                 <br><br>
                 <strong>Tea Breakって何？</strong><br>
@@ -80,7 +89,8 @@
                 Keemun(キームン)…中国茶です。<br>
                 ผักชี（パクチー)…誰も飲めないです。<br>
                 <br>
-                <img src="https://2.bp.blogspot.com/-2PgVP0iOkHY/USNoKzJtQlI/AAAAAAAANSI/1xMd2jtWmPA/s1600/cafe_tea.png">
+                <img
+                    src="https://2.bp.blogspot.com/-2PgVP0iOkHY/USNoKzJtQlI/AAAAAAAANSI/1xMd2jtWmPA/s1600/cafe_tea.png">
             </div>
         </div>
     </div>
