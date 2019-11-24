46c26b2b25e673daffad067e1cc3829ddd472f90
douro <piyopiyo51@gmail.comO>
2019-11-24 21:51:21 +0900

    テストコンテストの日時を修正

--- database/testcontest.sql
+++ database/testcontest.sql
@@ -1,6 +1,6 @@
-insert into contests values (0,"testcontest","2019-11-23 00:00:00","2009-08-24 00:00:00");
+insert into contests values (0,"testcontest","2019-11-23 00:00:00","2039-11-29 00:00:00");
 insert into problem values (0,"A",100,"/var/www/html/test/testcase");
-insert into problem values (0,"B",100,"/var/www/html/test/testcase");
-insert into problem values (0,"C",100,"/var/www/html/test/testcase");
-insert into problem values (0,"D",100,"/var/www/html/test/testcase");
-insert into problem values (0,"E",100,"/var/www/html/test/testcase");
+insert into problem values (0,"B",200,"/var/www/html/test/testcase");
+insert into problem values (0,"C",300,"/var/www/html/test/testcase");
+insert into problem values (0,"D",400,"/var/www/html/test/testcase");
+insert into problem values (0,"E",500,"/var/www/html/test/testcase");
